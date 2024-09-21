a457b3734b309d1c28055f06e09215c9262315d4
Cynthia <135084120+CynthiaLebelela@users.noreply.github.com>
2024-09-21 19:05:46 +0200

    script.js
    add functionality to the select then moved the functionality to index.html

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-
 function updateTime() {
     let joburgElement = document.querySelector("#johannesburg");
+    if (joburgElement) {
     let joburgDateElement = joburgElement.querySelector(".date");
     let joburgTimeElement = joburgElement.querySelector(".time");
     let joburbTime = moment().tz("Africa/Johannesburg");
     joburgDateElement.innerHTML = joburbTime.format("MMMM	Do YYYY");
     joburgTimeElement.innerHTML = joburbTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
+      "h:mm:ss [<small>]A[</small>]");
+    }
 
     let parisElement = document.querySelector("#paris");
+    if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
     let parisTimeElement = parisElement.querySelector(".time");
     let parisTime = moment().tz("Europe/Paris");
     parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
     parisTimeElement.innerHTML = parisTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-
+      "h:mm:ss [<small>]A[</small>]");
+    }
 }
 updateTime();
 setInterval(updateTime, 1000);
+
